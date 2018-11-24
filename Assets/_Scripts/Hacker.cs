c40c982a5139f8d2cca53489e3ef9217e6229942
DavidRood <shakiel@roadrunner.com>
2018-11-24 08:29:50 -0500

    added call functions for input

--- Assets/_Scripts/Hacker.cs
+++ Assets/_Scripts/Hacker.cs
@@ -1,26 +1,91 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
-    string greeting = "Hello Special Agent 73. \n\n";
+    int level;
 
 	// Use this for initialization
 	void Start ()
     {
+        Terminal.WriteLine("Server Hacker 2.0 \n");
+        Greeting("73 \n");
         ShowMainMenu();
     }
 
     void ShowMainMenu()
     {
-        Terminal.ClearScreen();
-        Terminal.WriteLine("Server Hacker 2.0 \n\n" + (greeting) + "System to hack? \n\n" + "1 - Public Library \n" + "2 - Police Department \n" + "3 - Federal Bureau of Investigation \n" + "4 - National Security Advisor \n");
+        // Terminal.ClearScreen();
+        Terminal.WriteLine ("System to hack? \n\n" + 
+                            "1 - Public Library \n" + 
+                            "2 - Police Department \n" + 
+                            "3 - Federal Bureau of Investigation \n" + 
+                            "4 - National Security Advisor \n");
     }
 
-    // Update is called once per frame
-    void Update ()
+    void Greeting(string greeting)
     {
-		
-	}
+        Terminal.WriteLine("Hello Special Agent " + greeting);
+    }
+
+    void OnUserInput(string input)
+    {
+        // Terminal.WriteLine("The user typed " + input);
+        if(input == "1")
+        {
+            level = 1;
+            StartGame();
+            return;
+        }
+        if (input == "2")
+        {
+            level = 2;
+            StartGame();
+            return;
+        }
+        if (input == "3")
+        {
+            level = 3;
+            StartGame();
+            return;
+        }
+        if (input == "4")
+        {
+            level = 4;
+            StartGame();
+            return;
+        }
+        if (input == "007")
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("Choose a valid level Mr. Bond.");
+            return;
+        }
+        if (input == "69")
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("The cone of silence is unavailable\nAgent 69.");
+            return;
+        }
+        if (input == "menu")
+        {
+            Terminal.ClearScreen();
+            ShowMainMenu();
+            return;
+        }
+        else
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("please choose a valid level.");
+        }
+    }
+
+    private void StartGame()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("You have chosen level " + level);
+
+    }
 }
