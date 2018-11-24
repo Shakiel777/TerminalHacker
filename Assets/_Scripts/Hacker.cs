dec937956cfba83f18e072aaea1d832e879b5b6f
DavidRood <shakiel@roadrunner.com>
2018-11-24 06:54:20 -0500

    new scene, created hacker.cs, added main menu

---
+++ Assets/_Scripts/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+    string greeting = "Hello Special Agent 73. \n\n";
+
+	// Use this for initialization
+	void Start ()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Server Hacker 2.0 \n\n" + (greeting) + "System to hack? \n\n" + "1 - Public Library \n" + "2 - Police Department \n" + "3 - Federal Bureau of Investigation \n" + "4 - National Security Advisor \n");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
